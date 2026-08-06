1f6f811e317d4e9fb922c1d753c08ae732983233
nehaklomate-byte <nehaklomate@gmail.com>
2026-08-06 16:30:46 +0530

    Add CookieConsent component to root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,6 +11,7 @@ import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
+import { CookieConsent } from "@/components/CookieConsent";
 
 function NotFoundComponent() {
   return (
@@ -153,6 +154,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <CookieConsent />
     </QueryClientProvider>
   );
 }
