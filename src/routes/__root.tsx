abfb8e23ce36294994ccb0140dce3c946929e202
nehaklomate-byte <nehaklomate@gmail.com>
2026-08-12 16:06:38 +0530

    Import isNativeAppShell in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,6 +13,7 @@ import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { CookieConsent } from "@/components/CookieConsent";
 import { InstallAppPrompt } from "@/components/InstallAppPrompt";
+import { isNativeAppShell } from "@/lib/platform";
 
 function ComingSoonPage() {
   return (
